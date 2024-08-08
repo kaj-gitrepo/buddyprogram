9cc581f1bf1a5fc52d4b2c41db3721f4ef087e59
Abdul Jabbar Karpoor(UST <184228@ust.com>
2024-08-08 18:38:17 +0530

    buddy program app initial commit

---
+++ src/test/java/com/ust/ticl/buddyprogram/BuddyprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.ticl.buddyprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
